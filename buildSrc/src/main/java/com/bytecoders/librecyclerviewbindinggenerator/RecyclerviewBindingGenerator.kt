aa061b56dc6c2160249b72772a5b3609344abb0b
bytecoders <josepantoni.torres@gmail.com>
2021-03-21 16:18:13 +0100

    Cleanup on app build.gradle and apply all project configuration from the plugin

--- buildSrc/src/main/java/com/bytecoders/librecyclerviewbindinggenerator/RecyclerviewBindingGenerator.kt
+++ buildSrc/src/main/java/com/bytecoders/librecyclerviewbindinggenerator/RecyclerviewBindingGenerator.kt
@@ -1,40 +1,46 @@
 package com.bytecoders.librecyclerviewbindinggenerator
 
-import com.android.build.api.dsl.AndroidSourceSet
 import com.android.build.gradle.internal.tasks.factory.dependsOn
 import com.bytecoders.librecyclerviewbindinggenerator.extensions.android
 import com.bytecoders.librecyclerviewbindinggenerator.extensions.packageToPath
+import com.bytecoders.librecyclerviewbindinggenerator.extensions.taskName
 import com.bytecoders.librecyclerviewbindinggenerator.extensions.variants
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import java.io.File
 
+private const val TASK_NAME = "generateRecyclerviewBinding"
+private const val OUTPUT_FILENAME = "RecyclerViewGeneratedBinding.kt"
 internal class RecyclerviewBindingGenerator : Plugin<Project> {
 
     override fun apply(project: Project) {
+        // Add dependency on compile
+        project.afterEvaluate { p ->
+            p.android().variants().all { variant ->
+                variant.javaCompileProvider.dependsOn(variant.taskName(TASK_NAME))
+            }
+        }
+
+        val compilePath = "${project.buildDir}/generated/source/recyclerview"
+
+        // Add Source sets
+        project.android().sourceSets { sourceSet ->
+            sourceSet.find { it.name == "main" }?.java?.srcDir(compilePath)
+        }
+
         project.android().variants().all { variant ->
 
             // Make a task for each combination of build type and product flavor
-            val bindingTaskName = "generateRecyclerviewBinding${variant.name.capitalize()}"
-
-            val packageFolder = BINDING_PACKAGE.packageToPath()
-            val outputPath = "${project.buildDir}/generated/source/recyclerview/$packageFolder"
+            val bindingTaskName = variant.taskName(TASK_NAME)
+            val outputPath = "$compilePath/${BINDING_PACKAGE.packageToPath()}"
 
             project.tasks.register(bindingTaskName, BindingTask::class.java) { bindingTask ->
                 bindingTask.group = "recyclerview"
 
-                // We write our output in the build folder. Also note that we want to have a
-                // reference to this so we can later mark it as a generated resource folder
+                // We write our output in the build folder
                 val outputDirectory =
                     File(outputPath).apply { mkdir() }
-                bindingTask.outputFile = File(outputDirectory, "RecyclerViewGeneratedBinding.kt")
-
-                // Marks the output directory as an app resource folder
-                variant.registerGeneratedResFolders(
-                    project.files(outputDirectory).builtBy(
-                        bindingTask
-                    )
-                )
+                bindingTask.outputFile = File(outputDirectory, OUTPUT_FILENAME)
             }
         }
     }
--- buildSrc/src/main/java/com/bytecoders/librecyclerviewbindinggenerator/extensions/Extensions.kt
+++ buildSrc/src/main/java/com/bytecoders/librecyclerviewbindinggenerator/extensions/Extensions.kt
@@ -2,13 +2,11 @@ package com.bytecoders.librecyclerviewbindinggenerator.extensions
 
 import com.android.build.gradle.AppExtension
 import com.android.build.gradle.BaseExtension
-import com.android.build.gradle.FeatureExtension
 import com.android.build.gradle.LibraryExtension
 import com.android.build.gradle.api.BaseVariant
 import org.gradle.api.DomainObjectSet
 import org.gradle.api.GradleException
 import org.gradle.api.Project
-import java.io.File
 
 fun Project.android(): BaseExtension {
     val android = project.extensions.findByType(BaseExtension::class.java)
@@ -34,3 +32,6 @@ fun BaseExtension.variants(): DomainObjectSet<out BaseVariant> {
 }
 
 fun String.packageToPath() = split(".").joinToString("/")
+
+@Suppress("DefaultLocale")
+fun BaseVariant.taskName(taskName: String): String = "$taskName${name.capitalize()}"
